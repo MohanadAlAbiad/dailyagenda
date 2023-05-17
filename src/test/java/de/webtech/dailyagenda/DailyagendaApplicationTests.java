1e5fec14b549e8137f30303fd5f3991035460c34
Mohanad Al-Abiad <weiss97m@gmail.com>
2023-05-17 21:08:30 +0200

    First Commit

---
+++ src/test/java/de/webtech/dailyagenda/DailyagendaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.webtech.dailyagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyagendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
